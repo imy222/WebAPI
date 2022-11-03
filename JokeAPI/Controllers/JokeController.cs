86ea1cc8870d17be076a3195624f542b2be86517
imy222 <imayyeo@yahoo.com>
2022-11-03 17:14:34 +1100

    Add Put Request

--- JokeAPI/Controllers/JokeController.cs
+++ JokeAPI/Controllers/JokeController.cs
@@ -1,17 +1,16 @@
-using JokeAPI.Data;
 using JokeAPI.Model;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore;
 
 namespace JokeAPI.Controllers;
 
 [ApiController]
 [Route("/joke")]
 
-public class JokeController : ControllerBase 
+public class JokeController : ControllerBase
 {
     //TODO Create local logger at later stage
-    
+
     /*private readonly ILogger<JokeController> _logger;
 
     public JokeController(ILogger<JokeController> logger)
@@ -21,30 +20,30 @@ public class JokeController : ControllerBase
 
     private readonly JokeContext _context;
 
-    public JokeController (JokeContext context)
+    public JokeController(JokeContext context)
     {
         _context = context;
         _context.Database.EnsureCreated();
     }
 
-    [HttpGet(Name = "GetAllJokes")]
+    [HttpGet("/test", Name = "GetAllJokes")]
     public IEnumerable<Joke> GetAllJokes()
     {
         return _context.Jokes.ToArray();
     }
-    
+
     //IAction Result and Ok, returns status code of API Call.
-    [HttpGet("/joke/random",Name = "GetOneRandomJoke")]
+    [HttpGet("/test/random", Name = "GetOneRandomJoke")]
     public IActionResult GetOneRandomJoke()
     {
         var random = new Random();
-        var randomJokeId = random.Next(1,_context.Jokes.Count()+1);
+        var randomJokeId = random.Next(1, _context.Jokes.Count() + 1);
         var randomJoke = _context.Jokes
             .Find(randomJokeId);
         return Ok(randomJoke);
     }
-    
-    [HttpGet("/joke/{id:int}", Name = "GetJokeById")]
+
+    [HttpGet("/test/{id:int}", Name = "GetJokeById")]
     public Joke GetSelectedJoke(int id)
     {
         var selectedJoke = _context.Jokes
@@ -52,24 +51,46 @@ public class JokeController : ControllerBase
         return selectedJoke ?? _context.Jokes.Last();
     }
 
-    [HttpPost]
+    /// Request without unit test
+
+    [HttpGet(Name = "GetJokes")]
+    public ActionResult<Joke> GetJokes()
+    {
+        return Ok(_context.Jokes.ToArray());
+    }
+
+    [HttpPost(Name = "PostOne")]
     public async Task<ActionResult<Joke>> PostJoke([FromBody] Joke joke)
     {
         _context.Jokes.Add(joke);
         await _context.SaveChangesAsync();
         return CreatedAtAction(
-            "GetAllJokes",
-            joke,
-            new Joke()
-            {
-                Id  = joke.Id,
-                Question = joke.Question,
-                Punchline = joke.Punchline,
-                CategoryId = joke.CategoryId,
-                Category = await _context.Categories.FindAsync(joke.CategoryId)
-            }
+            "GetJokes",
+            new {id = joke.Id},
+            joke
         );
     }
 
-
+    [HttpPost("{id}", Name = "Put")]
+    public async Task<ActionResult<Joke>> PutJoke(int id, [FromBody] Joke joke)
+    {
+        if (id != joke.Id)
+            return BadRequest();
+        
+        _context.Entry(joke).State = EntityState.Modified;
+    
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (await _context.Jokes.FindAsync(id) == null)
+            {
+                return NotFound();
+            }
+            throw;
+        }
+        return NoContent();
+    }
 }
