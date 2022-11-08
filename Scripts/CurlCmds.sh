6651c170bf2464b1fb8ab4f1f65f60ec8ef53057
imy222 <imayyeo@yahoo.com>
2022-11-08 16:05:45 +1100

    Update script due to refactoring of requests in JokeController

--- Scripts/CurlCmds.sh
+++ Scripts/CurlCmds.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash   
 
+LBLUE="\033[94m"
+ENDCOLOR="\033[0m"
+
 #Get all jokes
-echo "GET ALL JOKES"
+echo -e "\n\n${LBLUE}GET ALL JOKES ${ENDCOLOR}"
 curl https://localhost:7149/joke
 
-#Get one random joke
-echo
-echo
-echo "GET ONE RANDOM JOKE"
-curl https://localhost:7149/joke/random
-
 #Get one joke selected by joke ID
-echo
-echo
-echo "GET ONE JOKE SELECTED BY JOKE ID No 3"
+echo -e "\n\n${LBLUE}GET ONE JOKE SELECTED BY JOKE ID No 3 ${ENDCOLOR}"
 curl https://localhost:7149/joke/3
 
+
+#POST new joke
+echo -e "\n\n${LBLUE}POST ONE NEW JOKE ${ENDCOLOR}"
+curl -X 'POST' \
+  'https://localhost:7149/joke' \
+  -H 'accept: text/plain' \
+  -H 'Content-Type: application/json' \
+  -d '{
+        "Id" : 6,
+        "Question" : "What can Pikachu play with a baby?",
+        "Punchline" : "Pika-Boo!",
+        "CategoryId" : 1
+}'
