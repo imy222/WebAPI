097cbff548752de6f9abb8e9fd438eaf2e7aa254
imy222 <imayyeo@yahoo.com>
2022-12-05 15:09:28 +1100

    Add multi-stage dockerfile, docker-compose and update run test script

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -1,9 +1,5 @@
 #! /usr/bin/env bash
 set -euo pipefail
-# image name cannot contain capital letters
-image_name="joketests:1"
-# build image but only run up to test line in docker file. If additional lines added to 
-#docker file later, can add as another step in buildkite.yml 
-docker build -f Dockerfile.test . -t "${image_name}" --target "test"
 
-docker run "${image_name}"
+docker-compose up unit-tests
+docker-compose down
