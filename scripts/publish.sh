4eb127864c691e6bf57bbc503d6d2389022b1133
imy222 <imayyeo@yahoo.com>
2022-12-08 12:02:08 +1100

    Change docker-compose from up to build

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,7 +5,8 @@ image_name="docker.myob.com/future-makers-academy/imay-webapi"
 image_suffix=${BUILDKITE_BUILD_NUMBER}
 
 echo "Building application"
-docker-compose up --force-recreate publish 
+#docker-compose up --force-recreate publish 
+docker-compose build --no-cache publish
 
 echo "Pushing image to Cloudsmith"
 docker push $image_name:$image_suffix
