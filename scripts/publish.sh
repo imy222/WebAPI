4db28cde46d336cf502a28258ccf7eb526bd0f15
imy222 <imayyeo@yahoo.com>
2022-12-08 11:44:08 +1100

    Add docker compose and publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+set -euo pipefail
+
+image_name="docker.myob.com/future-makers-academy/imay-webapi"
+image_suffix=${BUILDKITE_BUILD_NUMBER}
+
+echo "Building application"
+docker-compose up --force-recreate publish 
+
+echo "Pushing image to Cloudsmith"
+docker push $image_name:$image_suffix
