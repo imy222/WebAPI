105f47bf7655a5d25e1691e40ba27383d51bd253
imy222 <imayyeo@yahoo.com>
2022-12-20 09:45:05 +1100

    Update imageTag

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 
 image_name="docker.myob.com/future-makers-academy/imay-webapi"
-imageTag="$(git rev-parse --short head)"
-#imageTag="latest"
+imageTag="$(git rev-parse --short origin/pipeline)"
 
 echo "Building application with Build: ${imageTag}"
 IMAGE_TAG=${imageTag} docker-compose build --no-cache publish
