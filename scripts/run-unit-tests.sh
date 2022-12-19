b1cceb881cab542558627b1576760fc7326e764f
imy222 <imayyeo@yahoo.com>
2022-12-19 13:36:21 +1100

    Add abort on container exit option

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 imageTag="${1}"
 
 echo "Build and run API unit test with Build#: ${imageTag}"
-IMAGE_TAG=${imageTag} docker-compose up --force-recreate unit-tests
-IMAGE_TAG=${imageTag} docker-compose down
+IMAGE_TAG=${imageTag} docker-compose up --force-recreate --abort-on-container-exit unit-tests
