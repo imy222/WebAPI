d29f22c8a3e4aa49fb742556405b2c57ca50bf85
imy222 <imayyeo@yahoo.com>
2023-01-20 11:15:28 +1100

    Change imageTag

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+imageTag="$(git rev-parse --short origin/pipeline)"
+
 echo "Deploy to Jupiter"
-ktmpl ./Deployment/template.yaml -f ./Deployment/default.yaml -p imageTag "$BUILDKITE_BUILD_NUMBER"  | kubectl apply -f - --validate=false
+ktmpl ./Deployment/template.yaml -f ./Deployment/default.yaml -p imageTag "${imageTag}"  | kubectl apply -f - --validate=false
 
