a184ba0602485ea6700f968678347656602c465b
imy222 <imayyeo@yahoo.com>
2023-01-23 13:35:37 +1100

    Test with invalid image Tag

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 imageTag="$(git rev-parse --short origin/pipeline)"
 
+die() { echo "$1"; exit "${2:-1}"; }
+
+[[ -z "$imageTag" ]] && die "Image Tag Is Empty" || echo "Valid Image Tag"
+
 echo "Deploy to Jupiter"
 ktmpl ./Deployment/template.yaml -f ./Deployment/default.yaml -p imageTag "${imageTag}"  | kubectl apply -f - --validate=false
-
