53bbeee369bd4a40773be57129a24ff0db07ebde
imy222 <imayyeo@yahoo.com>
2023-01-23 14:11:35 +1100

    Add error handling to check for empty imageTags

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-imageTag=""
-
+imageTag="$(git rev-parse --short origin/pipeline)"
+ 
 die() { echo "$1"; exit "${2:-1}"; }
 
 [[ -z "$imageTag" ]] && die "Image Tag Is Empty" || echo "Valid Image Tag"
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 image_name="docker.myob.com/future-makers-academy/imay-webapi"
 imageTag="$(git rev-parse --short origin/pipeline)"
+ 
+die() { echo "$1"; exit "${2:-1}"; }
+
+[[ -z "$imageTag" ]] && die "Image Tag Is Empty" || echo "Valid Image Tag"
 
 echo "Building application with Build: ${imageTag}"
 IMAGE_TAG=${imageTag} docker-compose build --no-cache publish
