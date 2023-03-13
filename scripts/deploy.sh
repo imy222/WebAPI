0f4b27dd0d3882720d499c8bbe82d7d7b70e77ce
imy222 <imayyeo@yahoo.com>
2023-03-13 19:39:45 +1100

    Update imageTag

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-imageTag="$(git rev-parse --short origin/pipeline)"
+imageTag="$(git rev-parse --short origin/development)"
  
 die() { echo "$1"; exit "${2:-1}"; }
 
